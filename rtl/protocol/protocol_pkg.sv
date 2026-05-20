0ed822986d2bd7274d0dc4c95f878ed2cc041b3b
TomaszWieclawski <twieclawski@student.agh.edu.pl>
2026-05-20 09:08:09 +0200

    protocol_pkg width mismatch fixed

--- rtl/protocol/protocol_pkg.sv
+++ rtl/protocol/protocol_pkg.sv
@@ -13,10 +13,10 @@ package protocol_pkg;
     
     //M VALUE COUNTING//
     //localparam int M_OVERSAMPLING_BITRATE
-    localparam int M_8X_100K = CLK400 / (8 * 100_000);
-    localparam int M_8X_1M   = CLK400 / (8 * 1_000_000);
-    localparam int M_16X_1M  = CLK400 / (16 * 1_000_000);
-    localparam int M_16X_5M  = CLK400 / (16 * 5_000_000);
+    localparam logic [15:0] M_8X_100K = CLK400 / (8 * 100_000);
+    localparam logic [15:0] M_8X_1M   = CLK400 / (8 * 1_000_000);
+    localparam logic [15:0] M_16X_1M  = CLK400 / (16 * 1_000_000);
+    localparam logic [15:0] M_16X_5M  = CLK400 / (16 * 5_000_000);
 
 
     //OVERSAMPLING TIMING WINDOWS//
